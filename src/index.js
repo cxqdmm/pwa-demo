158e18bb45623bd517e6cca803461baed4f7955f
cxqdmm <940909315@qq.com>
2017-12-29 09:58:42 +0800

    修改

--- src/index.js
+++ src/index.js
@@ -7,7 +7,6 @@ Main website JavaScript
 // unsupported browser
 if (!window.addEventListener || !window.history || !window.requestAnimationFrame || !document.getElementsByClassName) return;
 
-
 // enable service worker
 if ('serviceWorker' in navigator) {
   // register service worker
